996e60d9d9e61b209920f7adee5c73b7b176fbaf
Crew Spence <csspence123@gmail.com>
2019-05-27 17:12:45 -0500

    Added PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial',
+    script: './server/server_index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-173-235-60.compute-1.amazonaws.com',
+      key: '~/.ssh/tutorial.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com/csspence/reviews.git',
+      path: '/home/ubuntu/tutorial',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
