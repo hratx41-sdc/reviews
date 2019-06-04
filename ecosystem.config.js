10c5c519055c5127fe9212dbe534546ef03949c0
Surya Fotedar <suryafotedar@gmail.com>
2019-06-04 15:53:15 -0500

    Axios version and repo url updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-54-173-235-60.compute-1.amazonaws.com',
       key: '~/.ssh/tutorial.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/csspence/reviews.git',
+      repo: 'https://github.com/hratx41-sdc/reviews',
       path: '/home/ubuntu/tutorial',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
