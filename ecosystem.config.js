8bde62dedc3602512c5231b55ab83e5c977dff92
Surya Fotedar <suryafotedar@gmail.com>
2019-06-13 08:51:39 -0500

    updating for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [{
-    name: 'tutorial',
+    name: 'sdc-server',
     script: './server/server_index.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-54-173-235-60.compute-1.amazonaws.com',
-      key: '~/.ssh/tutorial.pem',
+      host: 'ec2-54-175-179-239.compute-1.amazonaws.com',
+      key: '~.ssh/psql-sdc-ec2.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/hratx41-sdc/reviews',
-      path: '/home/ubuntu/tutorial',
+      repo: 'https://github.com/hratx41-sdc/sf-fashova-reviews',
+      path: '/home/ubuntu/sf-fashova-reviews',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
