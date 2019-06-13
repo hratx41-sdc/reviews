c3abac4c90408e8d8a097c4a88e44fe1c78eca5c
Surya Fotedar <suryafotedar@gmail.com>
2019-06-13 09:06:14 -0500

    updated deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-54-175-179-239.compute-1.amazonaws.com',
       key: '~/Documents/GitHub/sf-fashova-reviews/psql-sdc-ec2.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/hratx41-sdc/sf-fashova-reviews',
+      repo: 'git@github.com:hratx41-sdc/sf-fashova-reviews.git',
       path: '/home/ubuntu/sf-fashova-reviews',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
